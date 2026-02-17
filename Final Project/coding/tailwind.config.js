aa267e9445100479be67958b09fb6effd199281f
Your Name <you@example.com>
2026-02-17 14:51:22 +0700

    Initial commit

---
+++ Final Project/coding/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // kalau file HTML-mu di folder lain, sesuaikan path-nya
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
